b9c55668cda00951df2c325d2bbe154a328fc36f
comsince <ljlong_2008@126.com>
2013-08-06 11:07:59 +0800

    播放页面增加点击歌曲播放功能

--- musicPlayer/src/com/comsince/knowledge/layout/MusicPlayerLocalLayout.java
+++ musicPlayer/src/com/comsince/knowledge/layout/MusicPlayerLocalLayout.java
@@ -2,17 +2,21 @@ package com.comsince.knowledge.layout;
 
 import java.util.List;
 
-import com.comsince.knowledge.MyApplication;
-import com.comsince.knowledge.R;
-import com.comsince.knowledge.adapter.LocalMusicListAdapter;
-import com.comsince.knowledge.entity.Music;
-
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+
+import com.comsince.knowledge.MyApplication;
+import com.comsince.knowledge.R;
+import com.comsince.knowledge.adapter.LocalMusicListAdapter;
+import com.comsince.knowledge.constant.Constant;
+import com.comsince.knowledge.entity.Music;
 
 public class MusicPlayerLocalLayout extends LinearLayout{
 	View rootview;
@@ -44,7 +48,22 @@ public class MusicPlayerLocalLayout extends LinearLayout{
 	}
 	
 	public void initListener(){
-		
+		localistview.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+				goplay(position);
+			}
+		});
+	}
+	
+	/**
+	 * 发送广播给播放service,通知其播放选中的音乐
+	 * */
+	public void goplay(int position) {
+		Intent intent = new Intent(Constant.ACTION_JUMR);
+		intent.putExtra("position", position);
+		context.sendBroadcast(intent);
 	}
 
 	public ListView getLocalistview() {
