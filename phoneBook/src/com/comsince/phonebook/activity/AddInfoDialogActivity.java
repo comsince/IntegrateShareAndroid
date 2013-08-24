4984159530411e510e9e4984d74c52c694401fdd
comsince <ljlong_2008@126.com>
2013-08-24 19:25:49 +0800

    通用添加消息框弹出界面

---
+++ phoneBook/src/com/comsince/phonebook/activity/AddInfoDialogActivity.java
@@ -0,0 +1,66 @@
+package com.comsince.phonebook.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.comsince.phonebook.R;
+
+public class AddInfoDialogActivity extends Activity implements OnClickListener {
+	public static final int ADD_INFO_COMMIT = 0;
+	public static final int ADD_INFO_CANCEL = 1;
+	String dialogTitle;
+    private TextView title;
+    private EditText infoInput;
+    private TextView addInfoCommit,addInfoCancel;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.addinfo_dialog);
+	    initView();
+	    setOnClickListener();
+	    dialogTitle = getIntent().getStringExtra("titleName");
+	}
+	public void initView(){
+		title = (TextView) findViewById(R.id.add_info_title);
+		if(!TextUtils.isEmpty(dialogTitle)){
+			title.setText("请输入"+dialogTitle);
+		}
+		infoInput = (EditText) findViewById(R.id.add_info_input);
+		addInfoCommit = (TextView) findViewById(R.id.add_info_commit);
+		addInfoCancel = (TextView) findViewById(R.id.add_info_cancel);
+	}
+	public void setOnClickListener(){
+		addInfoCommit.setOnClickListener(this);
+		addInfoCancel.setOnClickListener(this);
+	}
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.add_info_commit:
+			commitData();
+			break;
+		case R.id.add_info_cancel:
+			finish();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void commitData(){
+		String infoinput = infoInput.getText().toString().trim();
+		if(TextUtils.isEmpty(infoinput)){
+			Toast.makeText(this, "请输入信息", Toast.LENGTH_SHORT).show();
+		}else{
+			setResult(ADD_INFO_COMMIT, (new Intent()).setAction(infoinput));
+		}
+	}
+
+}
---
+++ phoneBook/src/com/comsince/phonebook/activity/PersonInfoActivity.java
@@ -0,0 +1,10 @@
+package com.comsince.phonebook.activity;
+
+import android.app.Activity;
+/**
+ * 登陆用户个人信息页面，在这个页面中登陆用户需要填写自己的基本信息，并向服务器提交，只有
+ * 提交个人信息的方能加入到群组中
+ * */
+public class PersonInfoActivity extends Activity {
+
+}
