f1e5ab9ae5415f98caca2aa490fa2093d8add539
廖金龙 <liaojinlong@meizu.com>
2014-11-01 10:41:11 +0800

    [AndroidC2DMDemo_stable]删除Demo项目

--- AndroidC2DMDemo_stable/src/org/joor/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * A copy of jOOR.
- * jOOR stands for Java Object Oriented Reflection. 
- * It is a simple wrapper for the java.lang.reflect package.
- * update this package from https://github.com/jOOQ/jOOR.
- */
-package org.joor;
